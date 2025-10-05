6f845e7d7c808dc6327d3cffe0a991412aa10280
ChinFromMY <150773562+ChinFromMY@users.noreply.github.com>
2025-10-05 18:48:03 +0800

    By using innerHTML to render the user input

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 let inputBox = []
 const inputEl = document.getElementById("input-el")
 
+//10. grab the unordered list and store it in a const variable 
+const ul = document.getElementById("ul-el")
+
 
 // 5. logout button clicked when user clicks save input button
 const inputButton = document.getElementById("btn")
@@ -13,8 +16,16 @@ inputButton.addEventListener("click", function(){
     //7. push the user input de value to array and log it
     // inputBox.push("www.awesomelead.com")
     // console.log(inputBox)
+    //8. get user input from input field
     inputBox.push(inputEl.value)
     console.log(inputBox)
 
 })
 
+//9. logout the items in inputBox array using for loop
+for(let i = 0; i < inputBox.length; i++){
+    // console.log(inputBox[i])
+    //10. render the inputBox in unordered list
+    //render the <li> elements
+    ul.innerHTML += "<li>" + inputBox[i] + "</li>"
+}
